b4e13280d34ce8a59aab964c4b902c21d8771c52
Mridul2021 <119924746+Mridul2021@users.noreply.github.com>
2023-07-21 20:27:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+var nodemailer = require('nodemailer');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+// Middleware for parsing form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Serve the HTML form page
+app.use(express.static("./"));
+
+// renders your index.html
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//mount your other paths
+// in this case render 404.
+app.get("*",function (req, res) {
+  res.status(404).send('<h1>File not found</h1>');
+});
+// Handle form submission and store the comment in a variable
+let commentData = ''; // Variable to store the comment
+
+app.post('/save-comment', (req, res) => {
+  commentData = req.body.comment;
+  res.send('Comment saved successfully');
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mridulsrivastava.vit2021@gmail.com',
+      pass: 'fqddomlyxncvnosx'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'youremail@gmail.com',
+    to: 'mridul.srivastava2021@vitstudent.ac.in',
+    subject: 'Sending Email using Node.js',
+    text: commentData
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+});
+
+// Route to retrieve the stored comment
+app.get('/get-comment', (req, res) => {
+  res.send(commentData);
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
+
